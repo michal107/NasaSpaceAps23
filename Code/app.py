d56e338f63b2f3bc68f7d2441d89f431b4a136f7
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2023-10-07 12:46:34 +0200

    db push

--- Code/app.py
+++ Code/app.py
@@ -3,26 +3,44 @@ import sqlalchemy_utils
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
+import sqlite3
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlalite///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+db = SQLAlchemy(app)
 
+class User(db.Model):
+     id = db.Column(db.Integer, primary_key=True, unique=True)
+     name = db.Column(db.String(20), unique=True)
+     mail = db.Column(db.String(50), unique=True)
+     pwd = db.Column(db.String(50))
 
 @app.route('/')
 def begin():
     return render_template("login.html")
-database={'Sophie':'123'}
 
 @app.route('/form_login',methods=['POST','GET'])
 def login():
     name1=request.form['username']
     pwd=request.form['password']
-    if name1 not in database:
+    if name1 not in db:
          return render_template('login.html',info='Invalid User')
     else:
-        if database[name1]!=pwd:
+        if db[name1]!=pwd:
             return render_template('login.html',info='Invalid Password')
         else:
 	         return render_template('home.html',name=name1)
+        
+@app.route('/form_register',methods=['POST'])
+def register():
+    name1=request.form['username']
+    pwd=request.form['password']
+    user_email=request.form['mail']
+    if name1 in db or user_email in db:
+         return render_template('register.html',info='User name or email already taken')
+    else:
+	    return render_template('home.html',name=name1)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
